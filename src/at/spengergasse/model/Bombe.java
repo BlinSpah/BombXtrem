1e0990f2acae96548066d321ab2927598a010cb8
genti <genti@David>
2017-04-29 16:44:51 +0200

    Anfang vom Model

---
+++ src/at/spengergasse/model/Bombe.java
@@ -0,0 +1,11 @@
+package at.spengergasse.model;
+
+public class Bombe {
+	private int bombenAnzahl;
+	private int range;
+	
+	public Bombe() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/at/spengergasse/model/Player.java
@@ -0,0 +1,23 @@
+package at.spengergasse.model;
+
+public class Player {
+	private boolean tot;
+	private Bombe bomb;
+	
+	public Player(boolean tot) {
+		// TODO Auto-generated constructor stub
+		setTot(tot);
+	}
+
+	
+	public boolean isTot() {
+		return tot;
+	}
+
+	public void setTot(boolean tot) {
+		this.tot = tot;
+	}
+
+	
+
+}
